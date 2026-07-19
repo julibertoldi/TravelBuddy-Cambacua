3f01d1737f3403c7638a509e9072545ce8d74bce
julibertoldi <bertoldijuliana1@gmail.com>
2026-07-19 10:49:32 -0300

    feat: incluir archivos de proxy necesarios para la integración con GeoDB

---
+++ angular/src/app/proxy/controllers/user-profile.service.ts
@@ -0,0 +1,29 @@
+import { RestService, Rest } from '@abp/ng.core';
+import { Injectable } from '@angular/core';
+import type { PublicUserProfileDto, UpdateUserProfileDto } from '../users/models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserProfileService {
+  apiName = 'Default';
+  
+
+  getPublicProfile = (userId: string, config?: Partial<Rest.Config>) =>
+    this.restService.request<any, PublicUserProfileDto>({
+      method: 'GET',
+      url: `/api/user-profile/${userId}`,
+    },
+    { apiName: this.apiName,...config });
+  
+
+  updateMyProfile = (input: UpdateUserProfileDto, config?: Partial<Rest.Config>) =>
+    this.restService.request<any, void>({
+      method: 'PUT',
+      url: '/api/user-profile/me',
+      params: { nombre: input.nombre, apellido: input.apellido, fotoPerfilUrl: input.fotoPerfilUrl, preferencias: input.preferencias },
+    },
+    { apiName: this.apiName,...config });
+
+  constructor(private restService: RestService) {}
+}
