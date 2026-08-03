bca64c61faea6d9ca6ebacf4e1ecad57c7e1da35
julibertoldi <bertoldijuliana1@gmail.com>
2026-08-03 17:06:47 -0300

    feat(admin): agregar AdminAppService y DTO para las estadisticas del dashboard

---
+++ src/TravelBuddy.Application.Contracts/Admin/IAdminAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace TravelBuddy.Admin
+{
+    public interface IAdminAppService : IApplicationService
+    {
+        Task<AdminDashboardStatsDto> GetDashboardStatsAsync();
+    }
+
+    public class AdminDashboardStatsDto
+    {
+        public long TotalUsers { get; set; }
+        public long TotalDestinations { get; set; }
+        public long TotalFavorites { get; set; }
+        public long ExternalApiErrorsCount { get; set; }
+    }
+}
---
+++ src/TravelBuddy.Application/Admin/AdminAppService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using TravelBuddy.Destinations;
+using TravelBuddy.Favorites;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Identity;
+
+namespace TravelBuddy.Admin
+{
+    public class AdminAppService : TravelBuddyAppService, IAdminAppService
+    {
+        private readonly IIdentityUserRepository _userRepository;
+        private readonly IRepository<Destination, Guid> _destinationRepository;
+        private readonly IRepository<Favorite> _favoriteRepository;
+
+        public AdminAppService(
+            IIdentityUserRepository userRepository,
+            IRepository<Destination, Guid> destinationRepository,
+            IRepository<Favorite> favoriteRepository)
+        {
+            _userRepository = userRepository;
+            _destinationRepository = destinationRepository;
+            _favoriteRepository = favoriteRepository;
+        }
+
+        public async Task<AdminDashboardStatsDto> GetDashboardStatsAsync()
+        {
+            var totalUsers = await _userRepository.GetCountAsync();
+            var totalDestinations = await _destinationRepository.GetCountAsync();
+            var totalFavorites = await _favoriteRepository.GetCountAsync();
+
+            return new AdminDashboardStatsDto
+            {
+                TotalUsers = totalUsers,
+                TotalDestinations = totalDestinations,
+                TotalFavorites = totalFavorites,
+                ExternalApiErrorsCount = 0
+            };
+        }
+    }
+}
