3f44ae5aef8afb4fb0585dc6b4621be678d76668
julibertoldi <bertoldijuliana1@gmail.com>
2026-08-03 22:25:23 -0300

    feat(admin): agregar AdminController para exponer metricas del dashboard

---
+++ src/TravelBuddy.HttpApi/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelBuddy.Admin;
+using TravelBuddy.Permissions;
+using Volo.Abp;
+
+namespace TravelBuddy.Controllers
+{
+    [Authorize(TravelBuddyPermissions.Admin.Default)]
+    [RemoteService]
+    [Route("api/app/admin")]
+    public class AdminController : TravelBuddyController, IAdminAppService
+    {
+        private readonly IAdminAppService _adminAppService;
+
+        public AdminController(IAdminAppService adminAppService)
+        {
+            _adminAppService = adminAppService;
+        }
+
+        [HttpGet("dashboard-stats")]
+        public async Task<AdminDashboardStatsDto> GetDashboardStatsAsync()
+        {
+            return await _adminAppService.GetDashboardStatsAsync();
+        }
+    }
+}
