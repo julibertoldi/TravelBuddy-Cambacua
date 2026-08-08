1c793e50e9530a554195635e3d4a6b46dcadaf9d
julibertoldi <bertoldijuliana1@gmail.com>
2026-08-08 13:24:02 -0300

    test(statistics): agregar pruebas unitarias para StatisticsAppService y finalizar modulo de monitoreo

---
+++ test/TravelBuddy.Application.Tests/Statistics/StatisticsAppService_Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Volo.Abp.Modularity;
+using Xunit;
+
+namespace TravelBuddy.Statistics
+{
+    public class StatisticsAppService_Tests : TravelBuddyApplicationTestBase<TravelBuddyApplicationTestModule>
+    {
+        private readonly IStatisticsAppService _statisticsAppService;
+
+        public StatisticsAppService_Tests()
+        {
+            _statisticsAppService = GetRequiredService<IStatisticsAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_Dashboard_Statistics()
+        {
+            var dashboardData = await _statisticsAppService.GetDashboardStatisticsAsync();
+
+            dashboardData.ShouldNotBeNull();
+            dashboardData.TopDestinations.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task Should_Get_Api_Call_Logs()
+        {
+            var logs = await _statisticsAppService.GetApiCallLogsAsync();
+            logs.ShouldNotBeNull();
+        }
+    }
+}
